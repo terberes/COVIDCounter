2be42a3c9a3b37e535655988e584f7007e0e3ed5
Kurlaev Nikita <yakuri2006@gmail.com>
2020-03-28 19:03:57 +0300

    Initial Commit

---
+++ COVIDCounter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  COVIDCounter
+//
+//  Created by Nikita Kurlaev on 28.03.2020.
+//  Copyright © 2020 Nikita Kurlaev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ COVIDCounter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  COVIDCounter
+//
+//  Created by Nikita Kurlaev on 28.03.2020.
+//  Copyright © 2020 Nikita Kurlaev. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
